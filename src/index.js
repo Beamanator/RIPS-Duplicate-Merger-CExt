c97e86e181b85f6c091344ab5a7702dc6f8913a0
Beamanator <dabeamanator@gmail.com>
2018-08-03 12:27:24 +0200

    Adding redux thunk

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import {
-    createStore, combineReducers
+    createStore, combineReducers, applyMiddleware, compose
 } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
 import ripsReducer from './store/reducers/rips';
 import portReducer from './store/reducers/port';
 import registerServiceWorker from './registerServiceWorker';
 
-// TODO: set window.__REDUX_...__ global variable set by
-// -> redux chrome extension (for production mode)
+// change to create-react-act dev environment specific code
+// process.env.NODE_ENV comes from config folder, env.js file
+// basically, if we're in development mode, show redux store, but if not
+// hide it from that extension.
+const composeEnhancers = process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null
+        || compose;
 
 // combine reducers
 const rootReducer = combineReducers({
@@ -19,7 +26,9 @@ const rootReducer = combineReducers({
     port: portReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 // apply redux store
 const app = (
