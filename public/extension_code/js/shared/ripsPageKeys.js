3d1891c1373c23e0ce96cda33c317431488fa205
Beamanator <dabeamanator@gmail.com>
2018-08-14 16:25:40 +0200

    commenting out page keys (may not be needed)

--- public/extension_code/js/shared/ripsPageKeys.js
+++ public/extension_code/js/shared/ripsPageKeys.js
@@ -1,18 +1,18 @@
-// loosely based off RIPS Auto Import CExt
+// // loosely based off RIPS Auto Import CExt
 
-// page names
-const RIPS_PAGE_KEYS = {
-    // client details
-    CLIENT_BASIC_INFORMATION: 'CLIENT_BASIC_INFORMATION',
-    ADDRESSES:  'ADDRESSES',
-    NOTES:      'NOTES',
-    ALIASES:    'ALIASES',
-    RELATIVES:  'RELATIVES',
-    CONTACTS:   'CONTACTS',
-    FILES:      'FILES',
-    PRIVATE_FILES: 'PRIVATE_FILES',
-    // case notes
-    HISTORY:    'HISTORY',
-    // maybe services
-    ADVANCED_SEARCH: 'ADVANCED_SEARCH'
-}
+// // page names
+// const RIPS_PAGE_KEYS = {
+//     // client details
+//     CLIENT_BASIC_INFORMATION: 'CLIENT_BASIC_INFORMATION',
+//     ADDRESSES:  'ADDRESSES',
+//     NOTES:      'NOTES',
+//     ALIASES:    'ALIASES',
+//     RELATIVES:  'RELATIVES',
+//     CONTACTS:   'CONTACTS',
+//     FILES:      'FILES',
+//     PRIVATE_FILES: 'PRIVATE_FILES',
+//     // case notes
+//     HISTORY:    'HISTORY',
+//     // maybe services
+//     ADVANCED_SEARCH: 'ADVANCED_SEARCH'
+// }
