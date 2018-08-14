1a4c60c49a75841a5d604358b1477032e21504c6
Beamanator <dabeamanator@gmail.com>
2018-08-14 16:24:43 +0200

    lowering field level in FIDs

--- public/extension_code/js/content_scripts/FIDs_ClientBasicInformation.js
+++ public/extension_code/js/content_scripts/FIDs_ClientBasicInformation.js
@@ -21,100 +21,97 @@ const {
 
 } = RIPS_FIELD_KEYS;
 
-// destructure page key(s)
-const { CLIENT_BASIC_INFORMATION } = RIPS_PAGE_KEYS;
-
 // loosely based off RIPS Auto Import CExt
 const FIELD_IDS_CLIENT_BASIC_INFORMATION = {
-    [CLIENT_BASIC_INFORMATION]: {
-        // ================== CURRENT CONTACT DETAILS: ==================
-        // ------ TEXTBOXES: ------
-        [FIRST_NAME]: 	'LFIRSTNAME',
-		[LAST_NAME]: 	'LSURNAME',
-        [PHONE_NUMBER]: 	'CDAdrMobileLabel',
-        [ADDRESS1]: 			'LADDRESS1',
-		[ADDRESS2]:				'LADDRESS2',
-        [OTHER_PHONE_NUMBER]: 	'CDAdrTelLabel',
-		[EMAIL_ADDRESS]: 		'CDLongField1',
+    // ================== CURRENT CONTACT DETAILS: ==================
+    // ------ TEXTBOXES: ------
+    [FIRST_NAME]: 	'LFIRSTNAME',
+    [LAST_NAME]: 	'LSURNAME',
+    [PHONE_NUMBER]: 	'CDAdrMobileLabel',
+    [ADDRESS1]: 			'LADDRESS1',
+    [ADDRESS2]:				'LADDRESS2',
+    [OTHER_PHONE_NUMBER]: 	'CDAdrTelLabel',
+    [EMAIL_ADDRESS]: 		'CDLongField1',
 
-        // ========================= BACKGROUND: ========================
-        // ------ TEXTBOXES: ------
-        [UNHCR_NUMBER]: 	'UNHCRIdentifier',
+    // ========================= BACKGROUND: ========================
+    // ------ TEXTBOXES: ------
+    [UNHCR_NUMBER]: 	'UNHCRIdentifier',
 
-        // ------ DROPDOWNS: ------
-        [DATE_OF_BIRTH]:	'LDATEOFBIRTH',
-		[GENDER]: 			'LGENDER',
-        [NATIONALITY]:		'LNATIONALITY',
-        [COUNTRY_OF_ORIGIN]: 	'LCOUNTRYOFORIGIN',
-        [ETHNIC_ORIGIN]: 		'LETHNICORIGIN',
-        [MAIN_LANGUAGE]:	'LMAINLANGUAGE',
-        [SECOND_LANGUAGE]: 		'LSECONDLANGUAGE',
-        [MARITAL_STATUS]: 		'LMARITALSTATUS',
-        
-        // =================== IMPORTANT INFORMATION: ==================
-        // ------ TEXTBOXES: ------
-        [IMPORTANT_INFORMATION]: 'LIMPORTANTINFO',
+    // ------ DROPDOWNS: ------
+    [DATE_OF_BIRTH]:	'LDATEOFBIRTH',
+    [GENDER]: 			'LGENDER',
+    [NATIONALITY]:		'LNATIONALITY',
+    [COUNTRY_OF_ORIGIN]: 	'LCOUNTRYOFORIGIN',
+    [ETHNIC_ORIGIN]: 		'LETHNICORIGIN',
+    [MAIN_LANGUAGE]:	'LMAINLANGUAGE',
+    [SECOND_LANGUAGE]: 		'LSECONDLANGUAGE',
+    [MARITAL_STATUS]: 		'LMARITALSTATUS',
+    
+    // =================== IMPORTANT INFORMATION: ==================
+    // ------ TEXTBOXES: ------
+    [IMPORTANT_INFORMATION]: 'LIMPORTANTINFO',
 
-        // ======================= URGENT NOTES: =======================
-        // ------ TEXTBOXES: ------
-        [URGENT_NOTES]:         'ClntPanic_PANIC_NOTES',
+    // ======================= URGENT NOTES: =======================
+    // ------ TEXTBOXES: ------
+    [URGENT_NOTES]:         'ClntPanic_PANIC_NOTES',
 
-        // ==================== OTHER INFORMATION: =====================
-        // ------ TEXTBOXES: ------
-        [APPOINTMENT_SLIP_NUMBER]:	'CDIdentifier1',
-		[CARITAS_NUMBER]:		'CDIdentifier2',
-		[CRS_NUMBER]:			'CDIdentifier3',
-		[IOM_NUMBER]: 			'CDIdentifier4',
-		[MSF_NUMBER]:			'CDIdentifier5',
-		[STARS_STUDENT_NUMBER]:	'CDIdentifier6',
+    // ==================== OTHER INFORMATION: =====================
+    // ------ TEXTBOXES: ------
+    [APPOINTMENT_SLIP_NUMBER]:	'CDIdentifier1',
+    [CARITAS_NUMBER]:		'CDIdentifier2',
+    [CRS_NUMBER]:			'CDIdentifier3',
+    [IOM_NUMBER]: 			'CDIdentifier4',
+    [MSF_NUMBER]:			'CDIdentifier5',
+    [STARS_STUDENT_NUMBER]:	'CDIdentifier6',
 
-        // ------ DROPDOWNS: ------
-        [RELIGION]:				'Dropdown1',
-		[UNHCR_STATUS]:			'Dropdown2',
-		[SOURCE_OF_REFERRAL]: 	'Dropdown3',
-		[CITY_OR_VILLAGE_OF_ORIGIN]: 		'Dropdown4',
-		[EMPLOYMENT_STATUS]: 	'Dropdown5',
-		[NEIGHBORHOOD]: 		'Dropdown6',
-		[HIGHEST_EDUCATION]: 	'Dropdown7',
+    // ------ DROPDOWNS: ------
+    [RELIGION]:				'Dropdown1',
+    [UNHCR_STATUS]:			'Dropdown2',
+    [SOURCE_OF_REFERRAL]: 	'Dropdown3',
+    [CITY_OR_VILLAGE_OF_ORIGIN]: 		'Dropdown4',
+    [EMPLOYMENT_STATUS]: 	'Dropdown5',
+    [NEIGHBORHOOD]: 		'Dropdown6',
+    [HIGHEST_EDUCATION]: 	'Dropdown7',
 
-        // ------ CHECKBOXES: ------
-        [CARE]: 		'IsCBLabel1',
-		[CRS]: 			'IsCBLabel2',
-		[EFRRA_OR_ACSFT]: 	'IsCBLabel3',
-		[IOM]:			'IsCBLabel4',
-		[MSF]:			'IsCBLabel5',
-		[PSTIC]:		'IsCBLabel6',
-		[REFUGE_EGYPT]:	'IsCBLabel7',
-		[SAVE_THE_CHILDREN]:	'IsCBLabel8',
-		[UNICEF_OR_TDH]: 			'IsCBLabel9',
-		[OTHER_SERVICE_PROVIDER]:	'IsCBLabel10',
+    // ------ CHECKBOXES: ------
+    [CARE]: 		'IsCBLabel1',
+    [CRS]: 			'IsCBLabel2',
+    [EFRRA_OR_ACSFT]: 	'IsCBLabel3',
+    [IOM]:			'IsCBLabel4',
+    [MSF]:			'IsCBLabel5',
+    [PSTIC]:		'IsCBLabel6',
+    [REFUGE_EGYPT]:	'IsCBLabel7',
+    [SAVE_THE_CHILDREN]:	'IsCBLabel8',
+    [UNICEF_OR_TDH]: 			'IsCBLabel9',
+    [OTHER_SERVICE_PROVIDER]:	'IsCBLabel10',
 
-        // -------- DATES: --------
-        [DATE_OF_ARRIVAL_IN_EGYPT]: 	'CDDateEntryCountryLabel',
-		[DATE_OF_UNHCR_REGISTRATION]: 	'CDDateRegisteredLabel',
-		[RSD_DATE]: 					'LRSDDATE',
-        // [technically a dropdown]
-        [LAST_RSD_UPDATE]:		'LPRIORITY',
+    // -------- DATES: --------
+    [DATE_OF_ARRIVAL_IN_EGYPT]: 	'CDDateEntryCountryLabel',
+    [DATE_OF_UNHCR_REGISTRATION]: 	'CDDateRegisteredLabel',
+    [RSD_DATE]: 					'LRSDDATE',
+    // [technically a dropdown]
+    [LAST_RSD_UPDATE]:		'LPRIORITY',
 
-        // ===================== VULNERABILITIES: ======================
-        // ------ CHECKBOXES: -----
-        // ... TODO: dynamic ...
-        // ------ TEXTBOXES: ------
-        [VULNERABILITY_NOTES]: 'DescNotes',
+    // ===================== VULNERABILITIES: ======================
+    // ------ CHECKBOXES: -----
+    // ... TODO: dynamic ...
+    // ------ TEXTBOXES: ------
+    [VULNERABILITY_NOTES]: 'DescNotes',
 
-        // ======================== DEPENDENTS: ========================
-        // ------ TEXTBOXES: ------
-        [FAMILY_SIZE]: 			'CDDependentStatsLabel1',
-        [UNHCR_CASE_SIZE]: 		'CDDependentStatsLabel2', // RLAP ONLY
-        [DIRECT_BENEFICIARIES]:	'CDDependentStatsLabel3', // PS ONLY
-        [INDIRECT_BENEFICIARIES]:	'CDDependentStatsLabel4', // PS ONLY
-    },
+    // ======================== DEPENDENTS: ========================
+    // ------ TEXTBOXES: ------
+    [FAMILY_SIZE]: 			'CDDependentStatsLabel1',
+    [UNHCR_CASE_SIZE]: 		'CDDependentStatsLabel2', // RLAP ONLY
+    [DIRECT_BENEFICIARIES]:	'CDDependentStatsLabel3', // PS ONLY
+    [INDIRECT_BENEFICIARIES]:	'CDDependentStatsLabel4', // PS ONLY
 
-    [PAGE_KEYS.ADDRESSES]: {
-        // TODO: dynamic from table...
-    },
+    // TODO: move to separate addresses script (FDIs_ADDRESSES)
+    // [PAGE_KEYS.ADDRESSES]: {
+    //     // TODO: dynamic from table...
+    // },
 
-    [PAGE_KEYS.NOTES]: {
-        [NOTES]: 'FREE_NOTES',
-    },
+    // TODO: move to separate notes script (FDIs_NOTES)
+    // [PAGE_KEYS.NOTES]: {
+    //     [NOTES]: 'FREE_NOTES',
+    // },
 }
