f2e24f83c245e5f3092a492290b62462b83cb734
Alex Beaman <dabeamanator@gmail.com>
2018-08-16 09:30:17 +0200

    Completed functionality (not tested - needs error handling)

--- public/extension_code/js/content_scripts/AdvancedSearchResults/Ctrl.js
+++ public/extension_code/js/content_scripts/AdvancedSearchResults/Ctrl.js
@@ -19,6 +19,48 @@ const analyzeSearchResult = ( clientNum ) => {
     Utils_Log(MESSAGE_SOURCE, 'Analyzing client num: ', clientNum);
     // TODO: complete this function
     debugger;
+
+    // TODO: think about waiting 1 - 4 seconds to wait for results
+    // -> to load (like Auto Import)
+
+    // 1) get all search result rows
+    const resultsSelector = FIELD_IDS_ADVANCED_SEARCH_RESULTS[SEARCH_RESULTS].selector;
+    const resultRows = document.querySelectorAll(resultsSelector);
+    // if length !== 1, something went wrong!
+    if (resultRows.length !== 1) {
+        // TODO: stop import
+        Utils_Error(
+            MESSAGE_SOURCE,
+            `Error - found something other than 1 result (len:${resultRows.length}):`,
+            resultRows
+        );
+        return;
+    }
+
+    // 2) found 1 result (THE client!) - click on it
+    resultRows[0].click();
+
+    // 3) get active client element
+    const activeClientFieldID = FIELD_IDS_ADVANCED_SEARCH_RESULTS[ACTIVE_CLIENT];
+    
+    // 4) wait for "Active Client" to match StARS #!
+    // -> try to find clientNum inside activeClientElem.value
+    Utils_WaitForCondition(
+        Utils_OnActiveClientMatches, {
+            activeClientSelector: '#' + activeClientFieldID,
+            clientNum: clientNum
+        }, 500, 6
+    )
+    .then(() => {
+        // 5) navigate to client's CBI page
+        const cbiTabSelector = FIELD_IDS_ADVANCED_SEARCH_RESULTS[TAB_CLIENT_BASIC_INFORMATION].selector;
+        cbiTabSelector.click();
+    })
+    .catch(errMsg => {
+        // TODO: stop import w/ error message!
+        Utils_Error(MESSAGE_SOURCE, 'Search Results ERROR', errMsg);
+        debugger;
+    });
 }
 
 // ===============================================================
