b6ef7bd8a25163db06b6607ca447a9e153aac479
Alex Beaman <dabeamanator@gmail.com>
2018-08-20 14:18:26 +0200

    collecing all cbi data!

--- public/extension_code/js/content_scripts/ClientBasicInformation/Ctrl.js
+++ public/extension_code/js/content_scripts/ClientBasicInformation/Ctrl.js
@@ -16,13 +16,34 @@ const port = chrome.runtime.connect({ name: PCs.PORTNAME_CS_CLIENT_BASIC_INFORMA
 //                         MAIN FUNCTIONS
 // ===============================================================
 const startImport = (clientNum) => {
-	// TODO: follow the following steps
+	// follow the following steps
 	// 1) get CBI's clientNum, make sure we're looking at the right
 	// -> client
-	debugger;
-	// 2.1) If yes, gather all the rest of the data
-	// 2.2) If no, error and fail the import
+	const starsNumFieldID = FIELD_IDS_CLIENT_BASIC_INFORMATION[STARS_NUMBER];
+	const starsNumElem = document.querySelector('#' + starsNumFieldID);
+	const starsNum = starsNumElem.value;
+
+	// 2.1) if client stars nums don't match, error and stop import
+	if (starsNum !== clientNum) {
+		let err = 'ERR: Somehow got to CBI page' +
+			' of wrong client!! StARS numbers don\'t match: ' +
+			`<${starsNum}> (from CBI) vs <${clientNum}> (from CExt)`;
+		Utils_Error(MESSAGE_SOURCE, err);
+		// TODO: send error back to bkg, stop import
+		return;
+	}
+
+	// 2.2) No issues! Gather all the rest of the data
+	// convert FID container into array
+	const data = Object.entries(FIELD_IDS_CLIENT_BASIC_INFORMATION)
+		// map field selectors to their field values
+		.map(([key, selector], i) => ({
+			key: document.querySelector('#' + selector).value
+		}));
+
 	// 3) data gathered, now send it back to background.js to store
+	debugger;
+
 	// 4) redirect to next page
 }
 
