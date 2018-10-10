b56ad2113753eda828ad840f0f2241f0156a4b19
Alex Beaman <dabeamanator@gmail.com>
2018-10-10 16:55:47 +0200

    filtered some service names out of action arr

--- public/extension_code/js/content_scripts/ViewActions/Ctrl.js
+++ public/extension_code/js/content_scripts/ViewActions/Ctrl.js
@@ -16,6 +16,22 @@ const port = chrome.runtime.connect({ name: PCs.PORTNAME_CS_VIEW_ACTIONS });
 //                         MAIN FUNCTIONS
 // ===============================================================
 const startMerge = ( mergeHistoryData ) => {
+    // First, filter out actions that are service-only
+    // -> like "Service started", "Service reopened"
+    // -> Note: other actions 'closed', 'closed - reopened at...'
+    // -> are already filtered out in History Ctrl.js
+    const actionsToFilter = [
+        'Service started',
+        'Service reopened',
+    ];
+    mergeHistoryData = mergeHistoryData.filter(action =>
+        // if action IS in 'actionsToFilter', return false (to remove)
+        !actionsToFilter.includes(action[ACTION_NAME])
+    );
+
+    // TODO: pop the first action off of the array, and store
+    // -> in background js, then redirect to add actions
+    // -> keep doing this until no more actions left to add! 
     console.log(mergeHistoryData);
     debugger;
 }
---
+++ public/extension_code/js/content_scripts/ViewActions/FIDs.js
@@ -0,0 +1,5 @@
+// destructure field keys
+const {
+    // just field names (no selectors used / needed)
+    ACTION_NAME,
+} = RIPS_FIELD_KEYS;
