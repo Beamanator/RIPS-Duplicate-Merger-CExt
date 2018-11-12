f550460e1892822e5efd1179198565098ac8ffa1
Alex Beaman <dabeamanator@gmail.com>
2018-11-12 13:44:18 +0200

    New custom dialog for warnings / notifications

---
+++ src/components/CustomDialog.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import {
+    Button,
+
+    Dialog, DialogActions, DialogContent,
+    DialogTitle, // DialogContentText,
+} from '@material-ui/core';
+
+const CustomDialog = (props) => {
+    const {
+        isOpen, handleClose, title,
+        dialogContent, buttonCloseText,
+        showActionButton, buttonActionText, buttonActionFunction,
+    } = props;
+
+    return (
+        <Dialog
+            open={isOpen}
+            onClose={handleClose}
+            aria-labelledby="merge-dialog-title"
+            aria-describedby="merge-dialog-description"
+        >
+            <DialogTitle id="merge-dialog-title">
+                {title}
+            </DialogTitle>
+            <DialogContent>
+                {dialogContent}
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    onClick={handleClose}
+                    color="primary"
+                >
+                    {buttonCloseText}
+                </Button>
+                {showActionButton ? <Button
+                    onClick={buttonActionFunction}
+                    color="primary" autoFocus
+                >
+                    {buttonActionText}
+                </Button> : null}
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default CustomDialog;
