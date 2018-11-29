b64c7fc0a456cce25d06ff46599edd923ca8d333
Alex Beaman <dabeamanator@gmail.com>
2018-11-29 10:42:37 +0200

    add err-bkg-code-not-recognized, archive-done

--- src/store/portCodes.js
+++ src/store/portCodes.js
@@ -14,6 +14,7 @@ export const ERROR_BKG_CODE_NOT_RECOGNIZED = 'ERROR_BKG_CODE_NOT_RECOGNIZED';
 //                                EXTERNAL CODES
 // ==============================================================================
 // --------------- receive -> background (BKG) to react app (RA) ----------------
+export const BKG_RA_ARCHIVE_DONE = 'BKG_RA_ARCHIVE_DONE';
 export const BKG_RA_ERROR_CODE_NOT_RECOGNIZED = 'BKG_RA_ERROR_CODE_NOT_RECOGNIZED';
 export const BKG_RA_IMPORT_DONE = 'BKG_RA_IMPORT_DONE';
 export const BKG_RA_INIT_PORT = 'BKG_RA_INIT_PORT';
@@ -21,6 +22,6 @@ export const BKG_RA_KILL_ALL = 'BKG_RA_KILL_ALL';
 export const BKG_RA_STOP_IMPORT_WITH_ERROR = 'BKG_RA_STOP_IMPORT_WITH_ERROR';
 
 // ----------------- send -> react app (RA) to background (BKG) -----------------
+export const RA_BKG_ERROR_BKG_CODE_NOT_RECOGNIZED = 'RA_BKG_ERROR_BKG_CODE_NOT_RECOGNIZED';
 export const RA_BKG_START_IMPORT = 'RA_BKG_START_IMPORT';
 export const RA_BKG_START_MERGE = 'RA_BKG_START_MERGE';
-export const RA_BKG_STOP_IMPORT = 'RA_BKG_STOP_IMPORT';
