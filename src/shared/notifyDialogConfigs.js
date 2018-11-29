73d0797f4578364fd92945b66514311311150b7e
Alex Beaman <dabeamanator@gmail.com>
2018-11-29 10:45:19 +0200

    Adding archiveDone config

--- src/shared/notifyDialogConfigs.js
+++ src/shared/notifyDialogConfigs.js
@@ -1,9 +1,20 @@
 const DEFAULT_BUTTON_CLOSE_TEXT = 'Exit';
 
+export const archiveDone = () => ({
+    title: 'Merge Complete!',
+    showActionButton: false,
+    buttonCloseText: DEFAULT_BUTTON_CLOSE_TEXT,
+    dialogContent: 'Merge process has completed! Please check ' +
+        'client records to make sure merge was successful! If you ' +
+        'find anything suspicious, please talk to Alex, the "RIPS ' +
+        'Guy" immediately! Or send an email to rips@stars-egypt.org.' +
+        ' Thanks!',
+});
+
 // config used to notify user that data import has finished
 export const importDone = () => ({
     title: 'Import Complete!',
-    showActionButton: true, // TODO: remove?
+    showActionButton: false, // TODO: keep this? default this??
     buttonActionText: 'Accept? Who knows?', // TODO: remove?
     buttonCloseText: 'Next',
     dialogContent: 'Import has finished. Next, select ' +
